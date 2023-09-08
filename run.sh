c26fb7cdbc4523bdca1771d3139df9cac0eceb38
NPU-Franklin <1109797240@qq.com>
2023-09-08 08:42:29 +0800

    :rocket: Update training bash script

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 exp_num="$1"
 shot_num="$2"
 data_type="$3"
-config_file=configs/eva-1b_convpass_exp"$exp_num"/in21k-eva1b_convpass_bs4_lr6e-4_"$shot_num"-shot_"$data_type".py
+config_file=configs/eva-1b_adapt_exp"$exp_num"/in21k-eva1b_adapt_bs4_lr6e-4_"$shot_num"-shot_"$data_type".py
 if test -z "$exp_num" || test -z "$shot_num" || test -z "$data_type"
 then
         echo "Missing parameter"
